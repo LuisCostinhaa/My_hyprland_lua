d0683f578b46f8a2997211bd9755514839c48905
LuisCostinhaa <costinhaa@gmail.com>
2026-08-08 16:39:48 +0100

    2026.08.08

--- copy_files.sh
+++ copy_files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p ~/PKG/My-Projects/My_hyprland_lua/config
-mkdir -p ~/PKG/My-Projects/My_hyprland_lua/sddm/Backgrounds
+mkdir -p ~/PKG/My-Projects/My_hyprland_lua/sddm
 cp -r ~/.zshrc ~/PKG/My-Projects/My_hyprland_lua/
 cp -r ~/.zshenv ~/PKG/My-Projects/My_hyprland_lua/
 cp -r ~/.bashrc ~/PKG/My-Projects/My_hyprland_lua/
@@ -10,7 +10,7 @@ sudo cp -r /etc/sddm.conf ~/PKG/My-Projects/My_hyprland_lua/
 sudo cp -r /etc/pacman.conf ~/PKG/My-Projects/My_hyprland_lua/
 sudo cp -r /etc/makepkg.conf ~/PKG/My-Projects/My_hyprland_lua/
 sudo cp -r /etc/default/grub ~/PKG/My-Projects/My_hyprland_lua/
-sudo cp -r /usr/share/sddm/themes/Sugar-Candy/Backgrounds ~/PKG/My-Projects/My_hyprland_lua/sddm/Backgrounds
+sudo cp -r /usr/share/sddm/themes/Sugar-Candy/Backgrounds/ ~/PKG/My-Projects/My_hyprland_lua/sddm/Backgrounds
 sudo cp -r /usr/share/sddm/themes/Sugar-Candy/theme.conf ~/PKG/My-Projects/My_hyprland_lua/sddm
 sudo cp -r ~/Imagens/backgrounds ~/PKG/My-Projects/My_hyprland_lua/
 cd ~/.config
